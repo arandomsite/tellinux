6108fb63a0c11b2b6c8315eee86bd5a6d6d965b1
ARandomSite <89870300+arandomsite@users.noreply.github.com>
2022-12-12 10:27:23 -0600

    Update browser.sh

--- browser.sh
+++ browser.sh
@@ -3,19 +3,33 @@
 main () {
   clear
   title="Tellinux - Home"
-  echo -e "\e[1;32;40m$title\e[0m"
-  echo "URL: (i.e. arandomsite)"
+  echo -e "\e[1;32;40m$title\e[0m"  
+  url
+}
+
+url () {
+echo "---
+URL: (i.e. arandomsite)"
   read url
-  giturl=${url//tellinux:}
+  giturl=${url%/*}
+  urlPath="${url#*/}"
+  if [[ ${url} != *"/"* ]]; then
+  	urlPath="index.txt"
+  fi
   if [ -d "tellinux-site" ]; then
   	rm -rf tellinux-site
   fi
   git clone https://github.com/$giturl/tellinux-site
-  clear
-  content=$(sudo sed '1d' tellinux-site/index.txt)
-  title=$(sudo head -1 tellinux-site/index.txt)
+  site
+}
+
+site () {
+clear
+  content=$(sudo sed '1d' tellinux-site/$urlPath)
+  title=$(sudo head -1 tellinux-site/$urlPath)
   echo -e "\e[1;32;40m$title\e[0m"
   echo "$content"
+  url
 }
 
 main
