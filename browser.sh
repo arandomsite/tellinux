f20ba084fa715f30b834ca24f36c3e89ff05eb4a
ARandomSite <89870300+arandomsite@users.noreply.github.com>
2022-12-12 10:11:01 -0600

    Update browser.sh

--- browser.sh
+++ browser.sh
@@ -7,12 +7,15 @@ main () {
   echo "URL: (i.e. arandomsite)"
   read url
   giturl=${url//tellinux:}
-  if [ -d "tellinux" ]; then
-  	rm -rf tellinux
+  if [ -d "tellinux-site" ]; then
+  	rm -rf tellinux-site
   fi
   git clone https://github.com/$giturl/tellinux-site
   clear
-  title=cat tellinux-site/README.md
-  echo $title
+  content=$(sudo sed '1d' tellinux-site/index.txt)
+  title=$(sudo head -1 tellinux-site/index.txt)
+  echo -e "\e[1;32;40m$title\e[0m"
+  echo "$content"
 }
+
 main
